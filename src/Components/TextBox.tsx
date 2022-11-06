a56079c838ba6accfdd3cd2a6bd2394340cf73cb
Kabir <seth.kabir14@gmail.com>
2022-11-06 11:59:30 +0530

    add quote library and random number generator

--- src/Components/TextBox.tsx
+++ src/Components/TextBox.tsx
@@ -1,26 +1,64 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+//the type of object we will get from the api call
+type Quote = {
+	text: string;
+	author: string;
+};
+
 const TextBox = () => {
+	//instead of using <Quote | null> we used {} as Quote because we know that
+	//quote will be assigned an object as soon as the app renders(useEffect) so it will not be null
+	const [quote, setQuote] = useState<Quote>({} as Quote);
+
+	//works when the button is clicked!
+	const handleClick = () => {
+		let randomNum: number = Math.floor(Math.random() * 1643 + 1);
+		axios
+			.get("https://type.fit/api/quotes")
+			.then((res) => {
+				setQuote(res.data[randomNum]);
+				console.log(randomNum);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
+	//use effect for when the app is first loaded (component did mount)
+	useEffect(() => {
+		let randomNum: number = Math.floor(Math.random() * 1643 + 1);
+
+		axios
+			.get("https://type.fit/api/quotes")
+			.then((res) => {
+				setQuote(res.data[randomNum]);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
 	return (
 		<div
 			id="quote-box"
 			className="absolute top-1/2 left-1/2 md:h-[400px] md:w-[750px] w-[350px] -translate-y-1/2 -translate-x-1/2 bg-red-200 p-2 space-y-3"
 		>
 			<div id="text" className="bg-blue-200 h-3/4 p-5 m-2">
-				<div className="text-xl text-justify p-2">
-					It is a long established fact that a reader will be
-					distracted by the readable content of a page when looking at
-					its layout. The point of using Lorem Ipsum is that it has a
-					more-or-less normal distribution of letters, as opposed to
-					using 'Content here, content here', making it look like
-					readable English. Many desktop publishing packages and web
-					page editors now use Lorem Ipsum as their default model
-					text, and a search for 'lorem ipsum'
-				</div>
+				<div className="sm:text-xl text-justify p-2">{quote?.text}</div>
 
 				<div id="author" className="text-right mr-8">
-					~author
+					~{quote?.author}
 				</div>
 			</div>
-			<button id="new-quote" className="absolute right-5 bg-green-50 p-3" >Click Here</button>
+			<button
+				id="new-quote"
+				onClick={handleClick}
+				className="absolute right-5 bg-green-50 p-3"
+			>
+				Click Here
+			</button>
 		</div>
 	);
 };
