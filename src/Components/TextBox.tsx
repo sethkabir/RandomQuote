0b0821aae4e5671866c21be29554a8224fc92011
Kabir <seth.kabir14@gmail.com>
2022-11-06 14:21:14 +0530

    add random color generator

--- src/Components/TextBox.tsx
+++ src/Components/TextBox.tsx
@@ -7,18 +7,28 @@ type Quote = {
 	author: string;
 };
 
+const backgroundColor: string[] = [
+	"bg-red-200",
+	"bg-green-200",
+	"bg-yellow-200",
+	"bg-blue-200",
+];
+
 const TextBox = () => {
 	//instead of using <Quote | null> we used {} as Quote because we know that
 	//quote will be assigned an object as soon as the app renders(useEffect) so it will not be null
 	const [quote, setQuote] = useState<Quote>({} as Quote);
+	const [color, setColor] = useState<string>("bg-violet-200");
 
 	//works when the button is clicked!
 	const handleClick = () => {
 		let randomNum: number = Math.floor(Math.random() * 1642 + 1);
+		let randomColor: number = Math.floor(Math.random() * 3 + 1);
 		axios
 			.get("https://type.fit/api/quotes")
 			.then((res) => {
 				setQuote(res.data[randomNum]);
+				setColor(backgroundColor[randomColor]);
 			})
 			.catch((err) => {
 				console.error(err);
@@ -42,7 +52,7 @@ const TextBox = () => {
 	return (
 		<div
 			id="quote-box"
-			className="absolute top-1/2 left-1/2 h-[300px] md:h-[300px] md:w-[750px] w-[350px] -translate-y-1/2 -translate-x-1/2 border-4 rounded-lg p-2"
+			className={`absolute top-1/2 left-1/2 h-[300px] md:h-[300px] md:w-[750px] w-[350px] -translate-y-1/2 -translate-x-1/2 border-4 rounded-lg p-2 ${color}`}
 		>
 			<div id="text" className="border-2 rounded-md h-44 p-5 m-2">
 				<div className="sm:text-xl text-justify p-2">{quote?.text}</div>
